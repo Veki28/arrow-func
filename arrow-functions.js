a600a4d876303d67f48d79767237fab13412d9b2
Vedran Lajic <vedran.lajic237@gmail.com>
2024-01-29 19:33:29 +0000

    practice w es6 n1

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,42 @@
 
 // Vanilla JavaScript Function
 
+//function addTwoNumbers(a, b) {
+// Code block
+//  return a + b
+//}
+//let sum = addTwoNumbers(3, 5)
+//console.log(sum)
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b
+}
+let sum = addTwoNumbers(3, 5)
+console.log(sum)
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => a + b
+let sum2 = addTwoNumbers2(6, 4)
+console.log(sum2)
+
 // Implicit Returns
 
+cosnt saySomething = message => console.log(message)
+saySomething('Hello there!')
+
+cosnt sayHello = () => console.log('hello')
+sayHello()
+
 // Returning Multiple Lines
+
+const returnMulti = () => (
+    `<p>
+    This do be a multiline stringg!
+    </p>`
+)
+
+console.log(returnMulti())
